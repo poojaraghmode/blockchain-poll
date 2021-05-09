1c6f3d6f639dae417eb4c859eae64a087ed920d4
Pooja Raghmode <poojaraghmode05@gmail.com>
2021-05-09 20:36:02 +0530

    Website Mockup

---
+++ src/app/types.ts
@@ -0,0 +1,12 @@
+export interface Poll{
+  id: number; //5
+  results: number[]; //[0, 0, 0, 0, 5, 7, 2]
+  question: string; //Which days of the week you like the most?
+  options: string[]; // ["Monday", "Tuesday", "wednesday" ... ]
+  image: string; //https://image.png
+}
+
+export interface Voter{
+  id: string; // 0x.....
+  voted: number[]; // [5]
+}
