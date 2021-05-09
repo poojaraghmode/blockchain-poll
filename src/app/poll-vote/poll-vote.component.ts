c93ed6f6230759de0a3060c4ea69daa465dc6e18
Pooja Raghmode <poojaraghmode05@gmail.com>
2021-05-10 01:19:27 +0530

    PollVote component results (charts)

--- src/app/poll-vote/poll-vote.component.ts
+++ src/app/poll-vote/poll-vote.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import ApexCharts from 'apexcharts';
 
 @Component({
   selector: 'app-poll-vote',
@@ -8,21 +9,57 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class PollVoteComponent implements OnInit {
 
+  @Input() voted: boolean;
+  @Input() options: string[];
+  @Input() results: number[];
+  @Input() question: string;
 
-  options=["Monday", "Tuesday", "Wednesday"]
+
   voteForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.voteForm = this.fb.group({
       selected: this.fb.control("", [Validators.required],)
     })
-   }
+  }
 
   ngOnInit(): void {
+    if(this.voted){
+      this.generateChart();
+    }
   }
 
   submitForm(){
     console.log(this.voteForm.value)
+  }
+
+  generateChart(){
+    const options: ApexCharts.ApexOptions = {
+      series: [
+        {
+          data: this.results,
+        },
+      ],
+      chart: {
+        height: 350,
+        type: 'bar',
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: '45%',
+          distributed: true,
+        },
+      },
+      legend: {
+        show: false,
+      },
+      xaxis:{
+        categories: this.options,
+      },
+    };
+
+    const chart = new ApexCharts(document.getElementById('poll-results'), options);
+    chart.render()
   }
 
 }
