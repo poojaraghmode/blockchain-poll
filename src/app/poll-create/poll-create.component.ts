30176507219e615c3023bcc175d3f297a0528327
Pooja Raghmode <poojaraghmode05@gmail.com>
2021-05-09 21:32:17 +0530

    PollCreate Component Logic

--- src/app/poll-create/poll-create.component.ts
+++ src/app/poll-create/poll-create.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-poll-create',
   templateUrl: './poll-create.component.html',
   styleUrls: ['./poll-create.component.scss']
 })
-export class PollCreateComponent implements OnInit {
+export class PollCreateComponent{
 
-  constructor() { }
+  pollForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.pollForm = this.fb.group({
+      question: this.fb.control('', Validators.required),
+      image: this.fb.control(''),
+      option1: this.fb.control(''),
+      option2: this.fb.control(''),
+      option3: this.fb.control(''),
+    });
+  }
+
+  submitForm(){
+    console.log(this.pollForm.value);
   }
 
 }
