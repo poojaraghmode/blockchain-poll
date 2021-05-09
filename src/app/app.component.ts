e139f93f6ce6b32f35ca4113ca6850832a41bc90
Pooja Raghmode <poojaraghmode05@gmail.com>
2021-05-10 00:37:36 +0530

    Poll Component Logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showForm = false;
+
+  polls = [
+    {
+      question: "Do you like Dogs or Cats?",
+      image: 'https://images.pexels.com/photos/46024/pexels-photo-46024.jpeg',
+      votes: [7,5,1],
+      voted: true,
+    },
+    {
+      question: "Whats your fav day?",
+      image: 'https://images.pexels.com/photos/1118448/pexels-photo-1118448.jpeg',
+      votes: [0,2,7],
+      voted: true,
+    },
+  ];
 }
--- src/app/poll/poll.component.ts
+++ src/app/poll/poll.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-poll',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PollComponent implements OnInit {
 
+  @Input() question: string;
+  @Input() votes: number[];
+  @Input() voted: boolean;
+  @Input() pollImage: string;
+
+  numberOfVotes: number;
+
   constructor() { }
 
   ngOnInit(): void {
+    if(this.votes.length){
+      this.numberOfVotes = this.votes.reduce((acc, curr)=>{
+        return (acc+=curr);
+      })
+    }
   }
 
 }
